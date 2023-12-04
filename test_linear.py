6024a601e0e678399d5b007e836d168bd6cd7cc4
Kenny Hung (he) <kenny.cfg@outlook.com>
2023-12-04 19:57:07 +0000

    Timer with disposible resource

--- test_linear.py
+++ test_linear.py
@@ -4,21 +4,30 @@ import unittest
 from linear import sum_of_integers
 
 
+class Timer:
+    def __enter__(self):
+        self.start = time.process_time()
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        end = time.process_time()
+        print(end - self.start)
+
+
 class TestLinear(unittest.TestCase):
     def test_for_1000000_numbers(self):
         source = list(range(0, 1000000))
-        start = time.process_time()
-        sum_of_integers(source)
-        end = time.process_time()
-        print(end - start)
+        with (Timer()):
+            sum_of_integers(source)
 
     def test_for_2000000_numbers(self):
         source = list(range(0, 2000000))
-        sum_of_integers(source)
+        with (Timer()):
+            sum_of_integers(source)
 
     def test_for_3000000_numbers(self):
         source = list(range(0, 3000000))
-        sum_of_integers(source)
+        with (Timer()):
+            sum_of_integers(source)
 
 
 if __name__ == '__main__':
