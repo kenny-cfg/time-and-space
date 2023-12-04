54aa4f6c5c18763ca2bf36255f30aa7585da428d
Kenny Hung (he) <kenny.cfg@outlook.com>
2023-12-04 20:01:06 +0000

    Timer with decorator

--- linear.py
+++ linear.py
@@ -1,6 +1,17 @@
+import time
 from typing import List
 
 
+def timer(func):
+    def inner(*args, **kwargs):
+        start = time.process_time()
+        func(*args, **kwargs)
+        end = time.process_time()
+        print(end - start)
+    return inner
+
+
+@timer
 def sum_of_integers(source: List[int]) -> int:
     sum_so_far = 0
     for x in source:
--- test_linear.py
+++ test_linear.py
@@ -16,18 +16,15 @@ class Timer:
 class TestLinear(unittest.TestCase):
     def test_for_1000000_numbers(self):
         source = list(range(0, 1000000))
-        with (Timer()):
-            sum_of_integers(source)
+        sum_of_integers(source)
 
     def test_for_2000000_numbers(self):
         source = list(range(0, 2000000))
-        with (Timer()):
-            sum_of_integers(source)
+        sum_of_integers(source)
 
     def test_for_3000000_numbers(self):
         source = list(range(0, 3000000))
-        with (Timer()):
-            sum_of_integers(source)
+        sum_of_integers(source)
 
 
 if __name__ == '__main__':
