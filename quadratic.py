5645fdae339a10bf7efb5c5fba054f67138ce8fe
Kenny Hung (he) <kenny.cfg@outlook.com>
2023-12-04 20:02:45 +0000

    Quadratic exercise

---
+++ quadratic.py
@@ -0,0 +1,7 @@
+# Write a function that takes a string
+# and outputs all two character strings where each character
+# is a character of the original string
+#
+# e.g. f('ab') should give ['aa', 'ab', 'ba', 'bb']
+#
+# Work out the time complexity of this function
